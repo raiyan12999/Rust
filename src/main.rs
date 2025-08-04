9970762f5313897c57d194e449d1af226067fe20
raiyan12999 <raiyanbinatik.m29@gmail.com>
2025-08-04 13:01:47 -0700

    use of for loop

--- src/main.rs
+++ src/main.rs
@@ -12,4 +12,12 @@ fn main() {
         println!("I am trying to learn Rust");
         i = i + 1;
     }
+
+    // code to add numbers from 1 to 100
+    let mut sum = 0;
+
+    for i in 1..100 {
+        sum += i;
+        println!("Sum: {}", sum);
+    }
 }
