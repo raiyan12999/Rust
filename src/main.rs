c87aaf1099bbf641667074d2ef1ce67a703371e8
raiyan12999 <raiyanbinatik.m29@gmail.com>
2025-08-07 11:06:38 -0700

    Type casting intro

--- src/main.rs
+++ src/main.rs
@@ -8,4 +8,18 @@ fn main(){
     println!("The value of x is now {}", x);
 
     // This became possible because of mut keyword. Otherwise Rust variables are immutable
+
+    // Type casting
+    // We use 'as' keyword for changing one variable of one type to another type
+
+    let decimal = 43.35;
+    println!("The decimal value of the variable is {}", decimal);
+
+    let integer = decimal as u64;
+    println!("The integer value of the variable is {}", integer);
+
+    let another_integer = decimal as i128;
+    println!("The new integer value of the variable is {}", another_integer);
+
+
 }
